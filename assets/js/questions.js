22db4a12eee7c8e5aea5b4ff4282b716135f36a1
sonictrain <nicola.brucoli92@gmail.com>
2023-12-31 19:49:46 +0100

    added questions object array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,62 @@
-console.log("questions.js file succesfully liked")
+const questions = [
+    {
+      id: 1,
+      question: "What is the name of Phoebe's most famous song?",
+      options: ["Smelly Cat", "Crazy Underwear", "Sticky Shoes", "Regina Phalange"],
+      answer: "Smelly Cat"
+    },
+    {
+      id: 2,
+      question: "What does Joey never share?",
+      options: ["Food", "His toothbrush", "His CDs", "His secrets"],
+      answer: "His toothbrush"
+    },
+    {
+      id: 3,
+      question: "In which city did Friends take place?",
+      options: ["New York", "Los Angeles", "Chicago", "Seattle"],
+      answer: "New York"
+    },
+    {
+      id: 4,
+      question: "What is the name of Ross and Monica's dog when they were kids?",
+      options: ["Max", "Rover", "Chi-Chi", "LaPooh"],
+      answer: "Chi-Chi"
+    },
+    {
+      id: 5,
+      question: "What is the name of Joey's stuffed penguin?",
+      options: ["Waddle", "Hugsy", "Flipper", "Mr. Ice"],
+      answer: "Hugsy"
+    },
+    {
+      id: 6,
+      question: "What is the name of Ross's second wife?",
+      options: ["Emily", "Carol", "Janice", "Rachel"],
+      answer: "Emily"
+    },
+    {
+      id: 7,
+      question: "What is Chandler's middle name?",
+      options: ["Michael", "Muriel", "Bing", "Francis"],
+      answer: "Muriel"
+    },
+    {
+      id: 8,
+      question: "Which character accidentally revealed Chandler and Monica's secret relationship?",
+      options: ["Rachel", "Phoebe", "Joey", "Ross"],
+      answer: "Rachel"
+    },
+    {
+      id: 9,
+      question: "What song makes Ross and Rachel's daughter, Emma, laugh for the first time?",
+      options: ["Twinkle, Twinkle, Little Star", "Baby Got Back", "I Will Always Love You", "The Lion Sleeps Tonight"],
+      answer: "Baby Got Back"
+    },
+    {
+      id: 10,
+      question: "Who mistakenly threw a woman's wooden leg into a fire at Thanksgiving?",
+      options: ["Joey", "Ross", "Monica", "Chandler"],
+      answer: "Joey"
+    },
+  ];
