942862dfbb6124b9ea1b60c2823f65a6395a65a0
sonictrain <nicola.brucoli92@gmail.com>
2024-01-03 02:55:53 +0100

    choicesDiv eventListener moved in the countdown function to be able to interact with the timer, added function to save the score in the local scorage and the function to display feedbackMessage which can dinamically show and hide message, customise the message to display and the timing before the message disappear, plus minor improvements

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,28 @@
 const timerEl = document.getElementById('time');
+const timerDiv = document.querySelector('#timer');
 const startQuiz = document.getElementById('start');
 const startScreen = document.getElementById('start-screen');
 const questionScreen = document.getElementById('questions');
 const endScreen = document.getElementById('end-screen');
 const questionTitle = document.getElementById('question-title');
 const choicesDiv = document.getElementById('choices');
+const initials = document.getElementById('initials');
+const submit = document.querySelector('#submit');
+const finalScore = document.getElementById('final-score');
+const feedback = document.getElementById('feedback');
+const storedScores = JSON.parse(localStorage.getItem("Scores"));
 
 let timeLeft = 60;
-
 let currentQuestionID = 1;
+
+let isSaved = false;
+
+let userObj = {
+    initials: "",
+    score: 0
+};
+
+let scoresObj = [];
 
 function countdown() {
 
@@ -26,6 +40,53 @@ function countdown() {
         }
 
     }, 1000);
+
+    choicesDiv.addEventListener("click", function (e) {
+
+        const element = e.target;
+
+        // If the element is a button
+        if (element.matches("button")) {
+            // render the new question based on the new question ID
+            questionObj = questions.find(q => q.id === currentQuestionID);
+
+            // handle extraTime base on the answer
+            if (element.dataset.answer === questionObj.answer) {
+                // show feedbackMessage for 750ms
+                feedbackMessage('show', "Correct! ✅", 750);
+                extraTime(15);
+            } else {
+                // show feedbackMessage for 750ms 
+                feedbackMessage('show', "Wrong! ❌", 750);
+                extraTime(-15);
+            }
+            timerEl.textContent = timeLeft;
+
+            // wipe choices container before populate it with the next question
+            choicesDiv.innerHTML = '';
+
+            if (currentQuestionID < 10) {
+
+                // increase question ID to display the next question
+                currentQuestionID++;
+                displayQuestion();
+
+            } else {
+
+                // stop timer
+                clearInterval(timeInterval);
+
+                // save score
+                userObj.score = timeLeft;
+                console.log(userObj.score);
+                finalScore.innerText = userObj.score;
+
+                // hide question screen and show end screen
+                questionScreen.classList.add('hide');
+                endScreen.classList.remove('hide');
+            }
+        }
+    });
 }
 
 function extraTime(s) {
@@ -54,43 +115,57 @@ function displayQuestion() {
 }
 
 startQuiz.addEventListener("click", function () {
-    startScreen.classList.remove('show');
     startScreen.classList.add('hide');
     questionScreen.classList.remove('hide');
-    questionScreen.classList.add('show');
+
+    isSaved = false;
 
     displayQuestion();
     countdown();
 })
 
-choicesDiv.addEventListener("click", function (e) {
+submit.addEventListener("click", function () {
 
-    const element = e.target;
+    // Return from function early if submitted initials is blank
+    if (initials.value.trim() !== "" && isSaved === false) {
+        userObj.initials = initials.value.trim();
+        isSaved = true;
 
-    // If the element is a button
-    if (element.matches("button")) {
-        // render the new question based on the new question ID
-        questionObj = questions.find(q => q.id === currentQuestionID);
+        saveScore(userObj);
+        window.location.href = 'highscores.html';
 
-        // handle extraTime base on the answer
-        element.dataset.answer === questionObj.answer ? extraTime(15) : extraTime(-15);
-
-        // wipe choices container before populate it with the next question
-        choicesDiv.innerHTML = '';
-
-        if (currentQuestionID < 10) {
-
-            // increase question ID to display the next question
-            currentQuestionID++;
-            displayQuestion();
-
-        } else {
-
-            // hide question screen and show end screen
-            questionScreen.classList.remove('show');
-            questionScreen.classList.add('hide');
-            endScreen.classList.remove('hide');
-            endScreen.classList.add('show');
-        }
+    } else {
+        return;
     }
 });
+
+function saveScore(s) {
+    if (storedScores !== null) {
+        scoresObj = storedScores;
+    }
+    scoresObj.push(s);
+    localStorage.setItem('Scores', JSON.stringify(scoresObj));
+};
+
+function feedbackMessage(action, text, millis) {
+
+    if (action === 'hide') {
+        // add 'hide' class
+        feedback.classList.add('hide');
+
+    } else if (action === 'show') {
+        // remove 'hide' class
+        feedback.classList.remove('hide');
+        // and hide again after a while
+        setTimeout(() => {            
+            feedback.classList.add('hide');
+        }, millis);
+
+    } else {
+        console.log("Error, please pass 'hide' or 'show' as first parameter.");
+    }
+
+    if (text) {
+        feedback.innerText = text;
+    }
+}
